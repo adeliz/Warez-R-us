0de643787445b918549b3716fd45f0a657cd1315
Blek <ghoulan@hotmail.com>
2018-05-03 15:12:10 +0200

    items

--- Desktop/warez3/Warez-R-Us/Warez-R-Us/Warez-R-Us/Item.cpp
+++ Desktop/warez3/Warez-R-Us/Warez-R-Us/Warez-R-Us/Item.cpp
@@ -2,11 +2,46 @@
 
 
 
-Item::Item()
-{
-}
 
+
+Item::Item(string name, string description, string brand, int weight, int price)
+{
+
+	this->name = name;
+	this->description = description;
+	this->brand = brand;
+	this->weight = weight;
+	this->price = price;
+}
 
 Item::~Item()
 {
+
+
 }
+
+string Item::getName() const
+{
+	return this->name;
+}
+
+string Item::getDescription() const
+{
+	return this->description;
+}
+
+string Item::getBrand() const
+{
+	return this->brand;
+}
+
+int Item::getWeight() const
+{
+	return this->weight
+}
+
+int Item::getPrice() const
+{
+	return this->price;
+}
+
--- Desktop/warez3/Warez-R-Us/Warez-R-Us/Warez-R-Us/Item.h
+++ Desktop/warez3/Warez-R-Us/Warez-R-Us/Warez-R-Us/Item.h
@@ -1,13 +1,30 @@
 #ifndef ITEM_H
 #define ITEM_H
+#include <string>
 
+using namespace std;
 
 class Item
 {
-public:
-	Item();
-	~Item();
-};
 
+private:
+
+	string name;
+	string description;
+	string brand;
+	int weight;
+	int price;
+
+public:
+	Item(string name, string description, string brand, int weight, int price);
+	~Item();
+
+	string getName()const;
+    string getDescription()const;
+	string getBrand()const;
+	int getWeight()const;
+	int getPrice()const;
+
+};
 
 #endif // !ITEM_H
